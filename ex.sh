4b669bf3aab414bde22709adb7d5a9cc9c2fe2c9
Younes202 <84544672+Younes202@users.noreply.github.com>
2023-01-25 22:57:55 +0100

    Finaly

--- ex.sh
+++ ex.sh
@@ -3,7 +3,8 @@
 interface="wlan0"
 duration=5
 
-
+rx_traffic=0
+tx_traffic=0
 # Prompt for username and password using Zenity
 username=$(zenity --entry --title="Authentication" --text="Enter your username:")
 password=$(zenity --entry --title="Authentication" --text="Enter your password:" --hide-text)
@@ -15,7 +16,6 @@ echo $password | su -c "whoami" - $username
 if [ $? -eq 0 ]; then
 zenity --info --title="Hello" --text="Hello, welcome : $username!"
 items=("Calcul a traffic" "1" "Cancel" "2")
-
 # Display the list of items using Zenity
 selected_item=$(zenity --list --title="Select an item" --column="Item" --column="Value" "${items[@]}" --print-column=2)
 
@@ -23,55 +23,71 @@ selected_item=$(zenity --list --title="Select an item" --column="Item" --column=
 	if [ $selected_item -eq "1" ]; then
     	# Display the selected item's value in a dialog box
   	 # zenity --info --title="Selected item" --text="You selected: $selected_item"
-       	# Show the available interfaces
+       	# L'affichage des interfaces qui existe dans votre réseau
 		interfaces=$(ip a | awk '/^[0-9]:/ {print $2}' | cut -d: -f1)
 		echo "Network interfaces on this system:"
 		echo $interfaces
 	
 	
-		# Select the interface to monitor
+		# Selectioner une interface 
 		read -p "Enter the name of the interface to monitor: " interface
-		#Rendre un fichier en mode temporaire .
-		rd= $(rm dd.dat)
-		#echo "Supprimer le fichier "+rd
-		ad= $(touch dd.dat)
-		#echo "Ajouter le fichier "+ad
-		#
-		# Une boucle pour faire des capture et les calcules 5 fois dans 5 seconde
-		for i in {1..5};do
-		# Get the initial network traffic values
+		#Rendre un fichier en mode temporaire via une methode que j'ai le fait , c'est quoi à chaque compilation du code les anciens données ils ont supprimer 
+
+		$(rm data.txt)
+				# par la suite on crée un autre  vierge pour le remplie 
+		$(touch  data.txt && chmod +777 data.txt)
+		
+	
+
+		# Une boucle pour faire des capture et les calcules 4 fois dans 5 seconde
+		for i in {2..5};do
+		# capture (Etat initial de signale ) 
 		initial_rx=$(cat /sys/class/net/$interface/statistics/rx_bytes)
 		initial_tx=$(cat /sys/class/net/$interface/statistics/tx_bytes)
 
-		# Wait for the specified duration
+		# Wait for
+	        # le temps restant entre les les deux tests
 		sleep $duration
 
-		# Get the final network traffic values
+		# capture (Etat final de signale ) 
 		final_rx=$(cat /sys/class/net/$interface/statistics/rx_bytes)
 		final_tx=$(cat /sys/class/net/$interface/statistics/tx_bytes)
 
-		# Calculate the traffic (received and transmitted)
+		# (received and transmitted) de Signale 
 		rx_traffic=$((final_rx - initial_rx))
 		tx_traffic=$((final_tx - initial_tx))
+		rx_traffic=$((rx_traffic / 1024))
+		tx_traffic=$(($tx_traffic / 1024))
 
 
 		echo "Network traffic for $interface over $duration seconds:"
 		# Print the results
 		if (($rx_traffic > $tx_traffic)); then
-		echo "Total: $((rx_traffic - tx_traffic ))  bytes"
-		echo  $((rx_traffic - tx_traffic ))  >> dd.dat
+		echo "Total: $((rx_traffic - tx_traffic ))  Kbytes"
+			
+		  tr=$((rx_traffic - tx_traffic ))
+
+		  
+		echo   $tr"	5"  >> data.txt
 		elif (($rx_traffic < $tx_traffic)); then
-		echo "Total: $((rx_traffic - tx_traffic ))  bytes"
-		echo  $(($tx_traffic - $rx_traffic)) >> dd.dat
+
+		
+		echo "Total:  $((tx_traffic - $tx_traffic ))  Kbytes"
+		  			  tr=$((tx_traffic - rx_traffic ))
+		echo   $tr"	5"  >> data.txt
 		else
-		echo "Total: $tx_traffic  bytes"
+		echo "Total: $tx_traffic  Kbytes"
+		     echo   $tx_traffic"	5" >> data.txt
                 fi
 		done 
-
-        
+		# Laison avec gnuplot  
+		$(gnuplot  plot.plt)
+		   # Laison avec HTml dashb
+		$(xdg-open  dashb.html)
+		
 
 	else 
-   	 	# Display an error message if no item was selected
+   	 	# Afficher une erreur si y acune selection 
 	result=$(zenity --question --text "Are you sure you want to quit?" --ok-button "Yes")
   		echo "User pressed in cancel button"
   	fi
@@ -79,4 +95,8 @@ selected_item=$(zenity --list --title="Select an item" --column="Item" --column=
 else
     zenity --error --title="Authentication" --text="Authentication failed."
 fi
+
+
+
+
 
